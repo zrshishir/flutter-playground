200cefca6b9d1693534e6c4e55c675415a2cd292
Ziaur Rahman <zrshishir@clementinedatabd.com>
2020-10-26 21:11:00 +0600

    pdf genaration

---
+++ pdf_genration/lib/screens/pdf_creation/pdf_creator_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:pdf_genration/screens/pdf_creation/report_pdf.dart';
+
+class PDFCreator extends StatefulWidget {
+  @override
+  _PDFCreatorState createState() => _PDFCreatorState();
+}
+
+class _PDFCreatorState extends State<PDFCreator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.only(top: 30),
+          height: 40,
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: Text(
+              'Get Report',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            color: Colors.blue,
+            onPressed: () {
+              reportView(context);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
