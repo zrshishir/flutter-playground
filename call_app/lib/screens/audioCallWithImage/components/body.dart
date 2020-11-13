763aea00120f9c84fa6ad72c211f6d68e282af4a
Ziaur Rahman <zrshishir@gmail.com>
2020-11-13 12:19:55 +0600

    adding title on the image

--- call_app/lib/screens/audioCallWithImage/components/body.dart
+++ call_app/lib/screens/audioCallWithImage/components/body.dart
@@ -6,10 +6,39 @@ class Body extends StatelessWidget {
     return Stack(
       fit: StackFit.expand,
       children: [
+        //set image
         Image.asset(
           'assets/images/full_image.png',
           fit: BoxFit.cover,
-        )
+        ),
+        //adding opacity
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.3),
+          ),
+        ),
+        //adding text upon on the image
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Jemmy \nWilliams",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3
+                      .copyWith(color: Colors.white),
+                ),
+                Text(
+                  "Incoming 00:01".toUpperCase(),
+                  style: TextStyle(color: Colors.white60),
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
